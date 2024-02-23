149df6266d3569d0f77290389084b81cc0468b8d
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-02-23 14:27:49 -0300

    first commit

---
+++ back/src/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+app.arguments(express.json())
+const port = 3000
+mongoose.connect('Crie um banco de dados no MongoDB Atlas e cole aqui a url')
+
+const Film = mongoose.model('Film', {
+    tittle: String,
+    description: String,
+    imageurl: String,
+    trailerurl: String
+});
+
+app.get("/", async (req, res) => {
+    const films = await Film.find()
+    return res.send(films)
+})
+
+app.post("/", async (req, res) => {
+    const film = new Film({
+        title: req.body.tittle,
+        description: req.body.description,
+        imageurl: req.body.imageurl,
+        trailerurl: req.body.trailerurl
+    })
+    await film.save()
+    return res.send(film)
+})
+
+app.delete("/:id", async(req, res) =>{
+    const film = await Film.findByIdAndDelete(req.params.id)
+    return res.send(film)
+})
+
+app.put('/:id', async(req, res) =>{
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title: req.body.tittle,
+        description: req.body.description,
+        imageurl: req.body.imageurl,
+        trailerurl: req.body.trailerurl
+    })
+    return res.send(film)
+})
+
+app.listen(port, () =>{
+    console.log(`A API está rodando na porta ${port}`)
+})
